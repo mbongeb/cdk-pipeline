4251932b3a23e4439b88fa8445dec0f7641baf42
mbongeb <eugene.monga@gmail.com>
2022-05-23 00:01:01 +0200

    added Pre-Prod stage

--- pipeline_webinar/pipeline_stack.py
+++ pipeline_webinar/pipeline_stack.py
@@ -2,30 +2,11 @@ import aws_cdk as cdk
 from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
 from constructs import Construct
 
+from .webservice_stage import WebServiceStage
+
 class PipelineStack(cdk.Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
-        # source_artifact = aws_codepipeline.Artifact()
-        # cloud_assembly_artifact = aws_codepipeline.Artifact()
-
-
-        # pipelines.CdkPipeline(self, "Pipeline",
-        #     cloud_assembly_artifact=cloud_assembly_artifact,
-        #     pipeline_name='WebinarPipeline',
-        #     source_action=aws_codepipeline_actions.GitHubSourceAction(
-        #         action_name='GitHub',
-        #         output=source_artifact,
-        #         oauth_token=Stack.SecretValue.secret_manager('github-token'),
-        #         repo='cdkpipeline',
-        #         owner='mbongeb',
-        #         trigger=aws_codepipeline_actions.GitHubTrigger.POLL),
-        #     synth_action=pipelines.SimpleSynthAction(
-        #         source_artifact=source_artifact,
-        #         cloud_assembly_artifact=cloud_assembly_artifact,
-        #         install_command='npm install -g aws-cdk && pip install -r requirements.txt',
-        #         synth_command='cdk synth'
-        #     ))
 
 
         pipeline = CodePipeline(self, "Pipeline",
@@ -39,3 +20,5 @@ class PipelineStack(cdk.Stack):
                                 "cdk synth"]
                     )
                 )
+
+        pipeline.add_stage(WebServiceStage(self, 'Pre-Prod', env=cdk.Environment(account="213911150042", region="us-west-2")))
